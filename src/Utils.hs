5ae6c2ce059683d5eadc8ddfe1001f1b4be4dd64
Radek Kozioł <radekoziol.it@gmail.com>
2018-01-12 13:43:35 +0100

    FreqWords fun improved

--- src/Utils.hs
+++ src/Utils.hs
@@ -19,5 +19,9 @@ splitToWords (x:xs)
 countOccurrence :: Eq a => a -> [a] -> Int
 countOccurrence x = length . filter (x == )
 
+-- For every word [how_many_times_it_occured,word]
+wordOccurence :: String -> [(Int,String)]
+wordOccurence text = map (\ str -> ( countOccurrence str (words text) ,str)) (nub $ words text)
+
 countAsciiOccurrence :: Int -> String -> Int
 countAsciiOccurrence ascii xs = length [x | x <- xs, fromEnum x == ascii || fromEnum x == ascii + 32]
