c5a5acc39fd67c771c0d6399ecfb75b4d2a1dfd2
Radek Kozioł <radekoziol.it@gmail.com>
2018-01-23 19:47:49 +0100

    most functions documentation added

--- src/Document.hs
+++ src/Document.hs
@@ -19,33 +19,63 @@ data Document = MkDocument
   , getContent :: String
   } deriving Show
 
-
--- |The 'getLines' fucntion calculates number of lines in given document
--- It takes one argument, of type Document and returns Int
+  
+{-|
+  The 'getLines' function returns number of lines in given document.
+  It takes one argument, of type Document and returns Int
+-}
 getLines :: Document -> Int
 getLines (MkDocument path content) = length $ lines content
 
+{-|
+  The 'getWords' function returns number of words in given document
+  It takes one argument, of type Document and returns Int
+-}
 getWords :: Document -> Int
 getWords (MkDocument path content) = length [x | x <- words content]
 
+{-|
+  The 'getWords' function returns number of non-repetetive words in given document
+  It takes one argument, of type Document and returns Int
+-}
 getUniqueWords :: Document -> Int
-getUniqueWords (MkDocument path content) = length $ group $ sort $ words content
+getUniqueWords (MkDocument path content) = length $ words content
 
--- Returns word if it at least duplicated
+{-|
+  The 'getWords' function returns a map of frequently used words [how_many_times_word_occured,word].
+  We define frequently as at least duplicated.
+  It takes one argument, of type Document and returns map of Int and String.
+-}
 getFreqWords :: Document -> [(Int, String)]
-getFreqWords (MkDocument path content) = filter (\(int, str) -> int > 1) (wordOccurence content)
+getFreqWords (MkDocument path content) = filter (\ (int,str) -> int > 1) (wordOccurrence content)
 
+{-|
+  The 'getChars' function returns a number of chars used in document.
+  It takes one argument, of type Document and returns map of Int.
+-}
 getChars :: Document -> Int
 getChars (MkDocument path content) = length content
 
+{-|
+  The 'getLettersOcc' function returns 
+  It takes one argument, of type Document and returns an array of Ints.
+-}
 getLettersOcc :: Document -> [Int]
 getLettersOcc (MkDocument path content) = loop 65 [] content
   where loop 91 result _                = result
         loop ascii result content       = loop (ascii + 1) (result ++ [countAsciiOccurrence ascii content]) content
 
+{-|
+  The 'getWordOcc' function counts occurence of given word in document.
+  It takes two arguments, of type Document and of type String and returns Int.
+-}        
 getWordOcc :: Document -> String -> Int
 getWordOcc (MkDocument path content) word = length [x | x <- words content, x == word]
 
+{-|
+  The 'getLongLines' function 
+  It takes two arguments, of type Document and of type Int and returns Int.
+-}        
 getLongLines :: Document -> Int -> Int
 getLongLines (MkDocument path content) n = loop 0 0 n content
   where loop result _ _ []               = result
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,30 +1,58 @@
 module Utils
-  ( countOccurrence
-  , wordOccurence
+  (
+    countOccurrence
+  , wordOccurrence
   , countAsciiOccurrence
   ) where
 
 import Data.Char
 import Data.List
 
+{-|
+  The 'countOccurrence' function counts occurence of some element in an array of elements.
+  In our project it is used to split text into words.
+  It takes two arguments, one is counted element of some type, other an array of thos types and return Int.
+-}
 countOccurrence :: Eq a => a -> [a] -> Int
 countOccurrence x = length . filter (x == )
 
+{-|
+  The 'removePunctuation' function removes in a string a comma or dot, placed at the end of string.
+  Note that it is assumed that any given text will follow those rules. 
+  It takes one arguments, of type String and returns String.
+-}
 removePunctuation :: String -> String
-removePunctuation x
+removePunctuation x 
         | (last x == '.' || last x == ',') = init x
         | otherwise                        = x
 
-removeNonAlpha :: [Char] -> [String]
+{-|
+  The 'removeNonAlpha' function for each word in text removes it's punctuation in order to 
+  count occurence of the same word - consider string "abc, abc."
+  It takes one arguments, of type String and returns an array of Strings.
+-}
+removeNonAlpha :: String -> [String]
 removeNonAlpha x = map (\x -> removePunctuation x) (words x)
 
+{-|
+  The 'lowerStrings' returns string in lower case.
+  It takes one arguments, of type an array of Strings and returns an array of Strings.
+-}
 lowerStrings :: [String] -> [String]
 lowerStrings x = map (\x -> map toLower x) x
 
--- For every word [how_many_times_it_occured,word]
-wordOccurence :: String -> [(Int,String)]
-wordOccurence text = map (\str -> (countOccurrence str processedtext, str)) (nub $ processedtext)
+{-|
+  The 'wordOccurence' function returns for a text, a map of [how_many_times_word_occured,word]
+  For each word in a set of processed text (in lower case, excluded punctuation), occurence is counted.
+  It takes one arguments, of type of String and returns a map of Int and String.
+-}
+wordOccurrence :: String -> [(Int,String)]
+wordOccurrence text = map (\ str -> (countOccurrence str processedtext ,str)) (nub $ processedtext)
       where processedtext = lowerStrings $ removeNonAlpha text
 
+{-|
+  The 'countAsciiOccurrence' function counts occurence of some ascii symbol in given string.
+  It takes two arguments, one is ascii element of type Int, second of type String, returns Int.
+-}
 countAsciiOccurrence :: Int -> String -> Int
 countAsciiOccurrence ascii xs = length [x | x <- xs, fromEnum x == ascii || fromEnum x == ascii + 32]
