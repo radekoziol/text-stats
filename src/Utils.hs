8a2bc8d860aec5e63fcded8337dad3b2dff6531c
logx <pkardas.it@gmail.com>
2018-01-23 11:58:46 +0100

    some test

--- src/Utils.hs
+++ src/Utils.hs
@@ -21,8 +21,8 @@ countOccurrence :: Eq a => a -> [a] -> Int
 countOccurrence x = length . filter (x == )
 
 -- For every word [how_many_times_it_occured,word]
-wordOccurence :: String -> [(Int,String)]
-wordOccurence text = map (\ str -> ( countOccurrence str (words text) ,str)) (nub $ words text)
+wordOccurence :: String -> [(Int, String)]
+wordOccurence text = map (\str -> (countOccurrence str (words text), str)) (nub $ words text)
 
 countAsciiOccurrence :: Int -> String -> Int
 countAsciiOccurrence ascii xs = length [x | x <- xs, fromEnum x == ascii || fromEnum x == ascii + 32]
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,9 +6,33 @@ main :: IO ()
 main = defaultMain unitTests
 
 unitTests =
-  testGroup
-    "splitToWords tests"
-    [splitToWords1]
+  testGroup "TEXT-STATS tests" [testSplitToWords1, testSplitToWords2,
+                                testCountOccurrence1,
+                                testWordOccurence1,
+                                testCountAsciiOccurrence1]
 
-splitToWords1 =
+testSplitToWords1 =
   testCase "Split Akademia Gorniczo-Hutnicza" $ assertEqual [] ["Akademia", "Gorniczo-Hutnicza"] (splitToWords "Akademia Gorniczo-Hutnicza")
+
+testSplitToWords2 =
+  testCase "Split Katedra informatyki . . ." $ assertEqual [] ["Katedra", "informatyki", ".", ".", "."] (splitToWords "Katedra informatyki . . .")
+
+testCountOccurrence1 =
+  testCase "Count occurrence of 'x'" $ assertEqual [] 3 (countOccurrence "x" ["x", "x", "x", "X"])
+
+testWordOccurence1 =
+  testCase "Count occurrence of words in 'The Glorious Glas...'" $ assertEqual [] [(1,"The"),
+                                                                                       (1,"Glorious"),
+                                                                                       (1,"Glasgow"),
+                                                                                       (1,"Haskell"),
+                                                                                       (1,"Compilation"),
+                                                                                       (1,"System,"),
+                                                                                       (2,"version"),
+                                                                                       (1,"8.2.2"),
+                                                                                       (1,"-"),
+                                                                                       (1,"the"),
+                                                                                       (1,"best")]
+                                                                                       (wordOccurence "The Glorious Glasgow Haskell Compilation System, version 8.2.2 - the best version")
+
+testCountAsciiOccurrence1 =
+  testCase "Count ascii occurrence" $ assertEqual [] 4 (countAsciiOccurrence 65 "ALA MA KOTA")
